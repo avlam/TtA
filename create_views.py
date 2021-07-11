9d5d2f7af5bb2b50d9a50d0326d3aa87e1ad5887
Alexander Lam <14320735+avlam@users.noreply.github.com>
2021-07-11 01:58:38 -0700

    change rank method for draws

--- create_views.py
+++ create_views.py
@@ -3,14 +3,14 @@
 
 # # Setup
 
-# In[3]:
+# In[1]:
 
 
 import pandas as pd
 from pathlib import Path
 
 
-# In[4]:
+# In[2]:
 
 
 staging_dir = Path(Path.cwd(),'staging')
@@ -19,13 +19,13 @@ analysis_dir = Path(Path.cwd(),'analysis')
 
 # # Load Data
 
-# In[9]:
+# In[3]:
 
 
 staging_tables = list(staging_dir.glob('*.csv'))
 
 
-# In[17]:
+# In[4]:
 
 
 data = {}
@@ -35,7 +35,7 @@ for table in staging_tables:
 
 # # Create View
 
-# In[22]:
+# In[5]:
 
 
 players_stack = pd.DataFrame(data['players'].stack(),columns=['players'])
@@ -43,27 +43,21 @@ scores_stack = pd.DataFrame(data['scores'].stack(),columns=['scores'])
 game_scores = players_stack.join(scores_stack)
 
 
-# In[27]:
+# In[6]:
 
 
 game_scores = game_scores.reset_index().rename(columns={'level_0':'game_id', 'level_1':'seat'})
 
 
-# In[29]:
+# In[7]:
 
 
-game_scores['place'] = game_scores.groupby('game_id')['scores'].rank(ascending=False)
-
-
-# In[30]:
-
-
-game_scores
+game_scores['place'] = game_scores.groupby('game_id')['scores'].rank(ascending=False, method='min')
 
 
 # # Save View
 
-# In[31]:
+# In[8]:
 
 
 game_scores.to_csv(analysis_dir.joinpath('game_scores.csv'))
