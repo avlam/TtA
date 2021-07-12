439f35fa823d8011e38ea4f1d0fc51f0cffe702b
Alexander Lam <14320735+avlam@users.noreply.github.com>
2021-07-11 23:24:29 -0700

    placeholder file to show what is expected in bgo credentials

---
+++ bgo_template.py
@@ -0,0 +1,6 @@
+# Placeholder file to show what is expected withing bgo.py. Replace with actual credentials when using on local machine, but should find a way to handle security more robustly moving forward.
+
+login_data = {
+    'identifiant': 'username', # username
+    'mot_de_passe': 'password' #password
+}
