be6636ef96e20f9f0ea6f6596614f3f4dec3de95
Alexander Lam <14320735+avlam@users.noreply.github.com>
2021-09-13 23:04:09 -0400

    add additional journal templates; treaty, tactics

--- journal_phrases.py
+++ journal_phrases.py
@@ -2,12 +2,12 @@ from parameters import SPACING_CHAR
 
 journal_phrases = {
     'impact': r'impact of (?P<impact_name>\w+)',
-    'production': fr'end turn[{SPACING_CHAR}]+?.*?(?P<player>\w+?) scores:'\
-        r'.*?(?P<food_gen>\d+?) food - consumption: (?P<consumption>\d+?) \(now (?P<food>\d+?)\)'\
-        r'.*?(?P<resource_gen>\d+?) resources \(now (?P<resource>\d+?)\)'\
-        r'.*?(?P<culture_gen>\d+?) culture \(now (?P<culture>\d+?)\)'\
-        r'.*?(?P<science_gen>\d+?) science \(now (?P<science>\d+?)\)'\
-        f'[{SPACING_CHAR}]+?',
+#     'production': fr'end turn[{SPACING_CHAR}]+?.*?(?P<player>\w+?) scores:'\
+#         r'.*?(?P<food_gen>\d+?) food - consumption: (?P<consumption>\d+?) \(now (?P<food>\d+?)\)'\
+#         r'.*?(?P<resource_gen>\d+?) resources \(now (?P<resource>\d+?)\)'\
+#         r'.*?(?P<culture_gen>\d+?) culture \(now (?P<culture>\d+?)\)'\
+#         r'.*?(?P<science_gen>\d+?) science \(now (?P<science>\d+?)\)'\
+#         f'[{SPACING_CHAR}]+?',
     'production_food': fr'end turn[{SPACING_CHAR}]+?.*?(?P<player>\w+?) scores:'\
         r'.*?(?P<food_gen>\d+?) food - consumption: (?P<consumption>\d+?) \(now (?P<food>\d+?)\)'\
         fr'.*?[{SPACING_CHAR}]+?',
@@ -32,9 +32,10 @@ journal_phrases = {
         r'.+? spends (?P<resources>\d+?) resource',
     'upgrades': r'(?P<player>\w+?) upgrades (?P<worker>\w+?)',
     'event': r'(?P<player>\w+?) plays event.*?scores (?P<event_age>\d+?) culture',
-    'tactics': fr'(?P<player>\w+?) (?P<action>.+?) tactics[{SPACING_CHAR}]+?(?P<age>\w+?) / (?P<tactic>\w+?)[{SPACING_CHAR}]+?',
-#     'population': ,
+    'tactics': fr'(?P<player>\w+?) (?P<action>.+?) tactics[{SPACING_CHAR}]+?(?P<age>\w+?) / (?P<tactic>.*?)[{SPACING_CHAR}]+?',
+    'population': r'(?P<player>\w+?) increases population.*? spends (?P<food>\d+?) food',
 #     'destroy': ,
-#     'treaty': ,
+    'treaty': fr'(?P<player>\w+?) proposes (?P<treaty>.*?) to (?P<recipient>\w+?)[{SPACING_CHAR}].*?(?P<a>\w+?) is A',
+    'treaty_response': fr'(?P<player>\w+?) (?P<response>\w+?) .*?offer[{SPACING_CHAR}]+?'
     'leader_elects': fr'(?P<player>\w+?) elects (?P<leader>.*?)[{SPACING_CHAR}]+?'
 }
